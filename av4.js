d15eaa1f7f084511a7082f7a985f19809e35e0d4
analuizast1212-crypto <analuizast1212@gmail.com>
2025-11-25 14:52:17 -0300

    av4

---
+++ av4.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync') ({sigint: true});
+const mensagens =[
+    {id: 1, frase:"hoje o dia esta lindo!"} ,
+    {id: 2, frase:"nao gostei desse trabalho"},
+    {id: 3, frase:"a aula foi muito divertida"},
+    {id:4, frase:"que tarefa chata"}
+]
+const resultado = mensagens.filter(u=>u.frase.includes("chata" , "nao gostei"))
+console.log(resultado)
